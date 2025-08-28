afee7d746f001eb3f4fe1a38419ce980298793a5
MaDr <97172549+MaDrCloudDev@users.noreply.github.com>
2025-08-27 20:52:39 -0700

    Revert "add cloudflare adapter"
    This reverts commit 225dba7ad302e654f45485587522d119e9b3c96d.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,5 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import cloudflare from '@astrojs/cloudflare';
-
 // https://astro.build/config
-export default defineConfig({
-  adapter: cloudflare()
-});
+export default defineConfig({});
